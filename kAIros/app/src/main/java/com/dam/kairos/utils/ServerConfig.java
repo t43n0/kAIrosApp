098660aa03c05f92c7de9d2b7ce74357bb273820
Luis <luisgallegoherrero1402@gmail.com>
2025-11-07 20:37:47 +0100

    docs: Anteproyecto (PDF)

---
+++ kAIros/app/src/main/java/com/dam/kairos/utils/ServerConfig.java
@@ -0,0 +1,8 @@
+package com.dam.kairos.utils;
+
+public class ServerConfig {
+    public static final String IMAGE_URL = "https://us-central1-diariogpt-95b2b.cloudfunctions.net/generateImage";
+    public static final String Analysis_URL = "https://us-central1-diariogpt-95b2b.cloudfunctions.net/analyzeWeek";
+
+
+}
